05376fb823ab6d9eac745d58ed89fb6f7b204c45
Afonso Carvalho <afonso.martingo.c@gmail.com>
2024-11-15 15:08:54 +0000

    PDF test

--- create_database.py
+++ create_database.py
@@ -1,11 +1,15 @@
 # from langchain.document_loaders import DirectoryLoader
-from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
 
 DATA_PATH = "data/pdf"
+CHROMA_PATH = "chroma"
 
 def load_documents():
-    loader = DirectoryLoader(DATA_PATH,glob="*.md")
+    
+    #loader = DirectoryLoader(DATA_PATH,glob="*.md")
+    loader = DirectoryLoader(DATA_PATH, glob="*.pdf", loader_cls=PyPDFLoader) # Load PDF files
     documents = loader.load()
     
     return documents
@@ -14,7 +18,7 @@ def split_text(documents: list[Document]):
 
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
-        chunck_overlap=500,
+        chunk_overlap=500,
         length_function=len,
         add_start_index=True,
     )
@@ -27,3 +31,7 @@ def split_text(documents: list[Document]):
     print(document.metadata)         # Print the metadata of the document
 
     return chunks
+
+
+documents = load_documents()
+split_text(documents)
