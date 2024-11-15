d6ed8edfc40e35964467737991da26be49c25f7f
Afonso Carvalho <afonso.martingo.c@gmail.com>
2024-11-15 18:44:27 +0000

    Vector Embedding database

--- create_database.py
+++ create_database.py
@@ -1,37 +1,74 @@
-# from langchain.document_loaders import DirectoryLoader
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+import time
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+import shutil
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Prompt the user for the API key
+OPENAI_API_KEY = input("Please enter your OpenAI API key: ")
+
+# Use the API key in your code
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 DATA_PATH = "data/pdf"
+DATA_PATH_articles = "data/articles"
 CHROMA_PATH = "chroma"
+CHROMA_PATH_articles = "chroma_articles"
 
-def load_documents():
-    
-    #loader = DirectoryLoader(DATA_PATH,glob="*.md")
-    loader = DirectoryLoader(DATA_PATH, glob="*.pdf", loader_cls=PyPDFLoader) # Load PDF files
+def main():
+    documents = load_documents(data_path=DATA_PATH)
+    documents_scientific = load_documents(data_path=DATA_PATH_articles)
+    chunks = split_text(documents)
+    chunks_scientific = split_text(documents_scientific)
+    save_to_chroma(chunks,CHROMA_PATH)
+    save_to_chroma(chunks_scientific,CHROMA_PATH_articles)
+
+def load_documents(data_path: str = DATA_PATH):
+    loader = DirectoryLoader(DATA_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
     documents = loader.load()
-    
     return documents
 
 def split_text(documents: list[Document]):
-
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
+        chunk_size=1500,
         chunk_overlap=500,
         length_function=len,
         add_start_index=True,
     )
 
-    chunks = text_splitter.split_documents(documents)   
+    chunks = text_splitter.split_documents(documents)
     print(f"Split {len(documents)} documents into {len(chunks)} chunks")
 
-    document = chunks[10]            # Get the 10th document
-    print(document.page_content)     # Print the content of the document
-    print(document.metadata)         # Print the metadata of the document
+    document = chunks[10]  # Get the 10th document
+    print(document.page_content)  # Print the content of the document
+    print(document.metadata)  # Print the metadata of the document
 
     return chunks
 
+def save_to_chroma(chunks: list[Document],chroma_path: str = CHROMA_PATH):
+    # Clear out the database first
+    if os.path.exists(chroma_path):
+        for _ in range(5):  # Retry up to 5 times
+            try:
+                shutil.rmtree(chroma_path)
+                break
+            except PermissionError:
+                print("PermissionError: Retrying in 1 second...")
+                time.sleep(1)
+        else:
+            print("Failed to delete the directory after 5 attempts.")
+            return
 
-documents = load_documents()
-split_text(documents)
+    db = Chroma.from_documents(chunks, OpenAIEmbeddings(), persist_directory=chroma_path)  # persist_directory is the directory where the database is saved
+    db.persist()  # Save the database
+    print(f"Saved {len(chunks)} chunks to {chroma_path}")
+
+if __name__ == "__main__":
+    main()
---
+++ query_data.py
@@ -0,0 +1,62 @@
+import argparse
+# from dataclasses import dataclass
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+import os
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Using exclusively the following contexts:
+
+1. Primary context (your thesis document):
+{context}
+
+2. Supplemental context (scientific articles from top-tier conferences that you need to follow the structure as reference):
+{external_context}
+
+---
+
+Compose a scientifically structured text on the topic below, suitable for submission to top-tier scientific conferences. The text should:
+
+1. Clearly and accurately explain the concept or technique.
+3. Include relevant analysis or discussion based on the context.
+4. Be well-structured and coherent.  
+
+Specific topic: {question}
+
+---
+
+Write the scientific text based on the provided context and follow all the guidelines given at the Specific topic:
+"""
+
+def main():
+    # Create CLI
+    parser = argparse.ArgumentParser(description="Query the database")
+    parser.add_argument("query_text", type=str, help="The query text")
+    args = parser.parse_args()  # Parse the arguments
+    query_text = args.query_text  # Get the query text
+
+    # Prompt the user for the API key
+    OPENAI_API_KEY = input("Please enter your OpenAI API key: ")
+
+    # Use the API key in your code
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+    # Prepare the DB
+    embedding_function = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    db = Chroma(persist_directory="chroma", embedding_function=embedding_function)
+
+    # Search the DB
+    results = db.similarity_search(query_text, k=3)  # Search the database for the most similar documents
+    if len(results) == 0:  # If no results are found
+        print("No results found")  # Print "No results found"
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc in results])  # Get the context text
+    print(context_text)  # Print the context text
+
+if __name__ == "__main__":
+    main()
